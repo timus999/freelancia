ef5df1820c432f83fe8ee5338848beb17504fe1c
timus <timus@localhost.localdomain>
2025-05-14 10:48:08 -0400

    feat(backend): setup axum project with base routes

---
+++ target/debug/deps/freelancia_backend-a19a78a7ecaf3a3d.d
@@ -0,0 +1,5 @@
+/home/timus/Desktop/rust_programming/blockchain/freelancia-backend/target/debug/deps/libfreelancia_backend-a19a78a7ecaf3a3d.rmeta: src/main.rs
+
+/home/timus/Desktop/rust_programming/blockchain/freelancia-backend/target/debug/deps/freelancia_backend-a19a78a7ecaf3a3d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/lazy_static-0d710c21aa4e1e5b.d
@@ -0,0 +1,6 @@
+/home/timus/Desktop/rust_programming/blockchain/freelancia-backend/target/debug/deps/liblazy_static-0d710c21aa4e1e5b.rmeta: /home/timus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs /home/timus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs
+
+/home/timus/Desktop/rust_programming/blockchain/freelancia-backend/target/debug/deps/lazy_static-0d710c21aa4e1e5b.d: /home/timus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs /home/timus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs
+
+/home/timus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/lib.rs:
+/home/timus/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.5.0/src/inline_lazy.rs:
